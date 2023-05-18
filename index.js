473a37a74fe5f5b6ab5926b349501e2f894e1916
Micah Geisel <micah@botandrose.com>
2023-05-18 11:59:16 +0200

    export non-verbed request function, too.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import { FetchRequest } from '@rails/request.js'
 
-const makeRequest = (verb, url, payload) => {
-  const request = new FetchRequest(verb, url, {
+const request = (verb, url, payload) => {
+  const req = new FetchRequest(verb, url, {
     headers: { Accept: "application/json" },
     body: payload,
   })
-  return request.perform().then(response => {
+  return req.perform().then(response => {
     if(response.response.headers.get('Content-Length') > 0) {
       return response.json
     }
   })
 }
 
-const get = (url, payload) => makeRequest('get', url, payload)
-const post = (url, payload) => makeRequest('post', url, payload)
-const put = (url, payload) => makeRequest('put', url, payload)
-const patch = (url, payload) => makeRequest('patch', url, payload)
-const destroy = (url, payload) => makeRequest('delete', url, payload)
+const get = (url, payload) => request('get', url, payload)
+const post = (url, payload) => request('post', url, payload)
+const put = (url, payload) => request('put', url, payload)
+const patch = (url, payload) => request('patch', url, payload)
+const destroy = (url, payload) => request('delete', url, payload)
 
-export { get, post, put, patch, destroy }
+export { request, get, post, put, patch, destroy }
