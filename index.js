37ab8b159ac19222aecc063967180c0002ded8b9
Micah Geisel <micah@botandrose.com>
2025-03-09 13:58:20 -0600

    allow payload to be ommited, and allow optional headers.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { FetchRequest } from '@rails/request.js'
 
-const request = (verb, url, payload) => {
+const request = (verb, url, payload, headers) => {
   const req = new FetchRequest(verb, url, {
-    headers: { Accept: "application/json" },
+    headers: { Accept: "application/json", ...headers },
     body: payload,
   })
   return req.perform().then(response => {
@@ -14,10 +14,10 @@ const request = (verb, url, payload) => {
   })
 }
 
-const get = (url, payload) => request('get', url, payload)
-const post = (url, payload) => request('post', url, payload)
-const put = (url, payload) => request('put', url, payload)
-const patch = (url, payload) => request('patch', url, payload)
-const destroy = (url, payload) => request('delete', url, payload)
+const get = (url, payload = {}, headers = {}) => request('get', url, payload, headers)
+const post = (url, payload = {}, headers = {}) => request('post', url, payload, headers)
+const put = (url, payload = {}, headers = {}) => request('put', url, payload, headers)
+const patch = (url, payload = {}, headers = {}) => request('patch', url, payload, headers)
+const destroy = (url, payload = {}, headers = {}) => request('delete', url, payload, headers)
 
 export { request, get, post, put, patch, destroy }
